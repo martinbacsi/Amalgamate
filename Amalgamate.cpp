85eda0c9d050a005a84e7c34ff17336792ead654
martinbacsi <csaknorrisz@gmail.com>
2016-11-25 11:49:15 +0100

    automatikus file javulás

--- Amalgamate.cpp
+++ Amalgamate.cpp
@@ -28,6 +28,10 @@
 
 #include <regex>
 #include <unordered_map>
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
 
 #include "juce_core_amalgam.h"
 
@@ -536,7 +540,7 @@ private:
             if (matchesWildcard (parsedInclude.filename.replaceCharacter ('\\', '/'))
               && ! includesToIgnore.contains (targetFile.getFileName()))
             {
-              String fileNamePart = File (parsedInclude.filename).getFileName ();
+              String fileNamePart = targetFile.getFileName ();
               RemapTable::iterator remapTo = m_remapTable.find (std::string (fileNamePart.toUTF8 ()));
               if (level != 0 && remapTo != m_remapTable.end ())
               {
@@ -681,234 +685,171 @@ private:
 /******************************************************************************/
 
 //==============================================================================
-int main (int argc, char* argv[])
+
+
+
+
+
+
+
+
+void RefreshDirectory(LPTSTR);
+void RefreshTree(LPTSTR);
+void WatchDirectory(LPTSTR);
+
+
+
+
+
+
+
+
+
+int main(int argC, char** argV)
 {
-  bool error = false;
-  bool usage = true;
+	//char* dir = "D:\\Amalgamate\\Builds\\VisualStudio2010\\";
+	//char pwd[100];
+	//GetCurrentDirectory(100, pwd);
 
-  const String name (argv[0]);
+	String homoDir = argV[1];
+	String inFile = homoDir + String("CG.cpp");
+	String outFile = homoDir + String("CG_OUT.cpp");
 
-  Amalgamator amalgamator (name);
-  bool gotCheckSystem = false;
-  bool gotWildcards = false;
-  bool gotTemplate = false;
-  bool gotTarget = false;
 
-  for (int i = 1; i < argc; ++i)
-  {
-    String option (argv[i]);
+	Amalgamator amalgamator(inFile);
+	amalgamator.setTemplate(inFile);
+	amalgamator.setTarget(outFile);
+	while (1)
+	{
+		WatchDirectory(argV[1]);
+		
+		amalgamator.process();
+	}
+}
 
-    String value;
-    if (i + 1 < argc)
-      value = String (argv[i+1]).unquoted ();
 
-    if (option.compareIgnoreCase ("-i") == 0)
-    {
-      ++i;
-      if (i < argc)
-      {
-        amalgamator.addDirectoryToSearch (value);
-      }
-      else
-      {
-        std::cout << name << ": Missing parameter for -i\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-f") == 0)
-    {
-      ++i;
-      if (i < argc)
-      {
-        amalgamator.addForceReinclude (value);
-      }
-      else
-      {
-        std::cout << name << ": Missing parameter for -f\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-p") == 0)
-    {
-      ++i;
-      if (i < argc)
-      {
-        amalgamator.addPreventReinclude (value);
-      }
-      else
-      {
-        std::cout << name << ": Missing parameter for -p\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-d") == 0)
-    {
-      ++i;
-      if (i < argc)
-      {
-        if (value.contains ("="))
-        {
-          String name = value.upToFirstOccurrenceOf ("=", false, false);
-          String define = value.fromFirstOccurrenceOf ("=", false, false);
+void WatchDirectory(LPTSTR lpDir)
+{
+	DWORD dwWaitStatus;
+	HANDLE dwChangeHandles[2];
+	TCHAR lpDrive[4];
+	TCHAR lpFile[_MAX_FNAME];
+	TCHAR lpExt[_MAX_EXT];
 
-          amalgamator.addDefine (name, define);
-        }
-        else
-        {
-          std::cout << name << ": Incorrect syntax for -d\n";
-        }
-      }
-      else
-      {
-        std::cout << name << ": Missing parameter for -d\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-w") == 0)
-    {
-      ++i;
-      if (i < argc)
-      {
-        if (!gotWildcards)
-        {
-          amalgamator.setWildcards (value);
-        }
-        else
-        {
-          std::cout << name << ": Duplicate option -w\n";
-          error = true;
-          break;
-        }
-      }
-      else
-      {
-        std::cout << name << ": Missing parameter for -w\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-s") == 0)
-    {
-      if (!gotCheckSystem)
-      {
-        amalgamator.setCheckSystemIncludes (true);
-        gotCheckSystem = true;
-      }
-      else
-      {
-        std::cout << name << ": Duplicate option -w\n";
-        error = true;
-        break;
-      }
-    }
-    else if (option.compareIgnoreCase ("-v") == 0)
-    {
-      amalgamator.setVerbose ();
-    }
-    else if (option.startsWith ("-"))
-    {
-      std::cout << name << ": Unknown option \"" << option << "\"\n";
-      error = true;
-      break;
-    }
-    else
-    {
-      if (!gotTemplate)
-      {
-        amalgamator.setTemplate (option);
-        gotTemplate = true;
-      }
-      else if (!gotTarget)
-      {
-        amalgamator.setTarget (option);
-        gotTarget = true;
-      }
-      else
-      {
-        std::cout << name << ": Too many arguments\n";
-        error = true;
-        break;
-      }
-    }
-  }
+	_tsplitpath_s(lpDir, lpDrive, 4, NULL, 0, lpFile, _MAX_FNAME, lpExt, _MAX_EXT);
 
-  if (gotTemplate && gotTarget)
-  {
-    usage = false;
+	lpDrive[2] = (TCHAR)'\\';
+	lpDrive[3] = (TCHAR)'\0';
 
-    //amalgamator.print ();
-    error = amalgamator.process ();
-  }
-  else
-  {
-    if (argc > 1)
-      std::cout << name << " Too few arguments\n";
+	// Watch the directory for file creation and deletion. 
 
-    error = true;
-  }
+	dwChangeHandles[0] = FindFirstChangeNotification(
+		lpDir,                         // directory to watch 
+		FALSE,                         // do not watch subtree 
+		FILE_NOTIFY_CHANGE_FILE_NAME); // watch file name changes 
 
-  if (error && usage)
-  {
-    std::cout << "\n";
-    std::cout << "  NAME" << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "   " << name << " - produce an amalgamation of C/C++ source files." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "  SYNOPSIS" << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "   " << name << " [-s]" << "\n";
-    std::cout << "     [-w {wildcards}]" << "\n";
-    std::cout << "     [-f {file|macro}]..." << "\n";
-    std::cout << "     [-p {file|macro}]..." << "\n";
-    std::cout << "     [-d {name}={file}]..." << "\n";
-    std::cout << "     [-i {dir}]..." << "\n";
-    std::cout << "     {inputFile} {outputFile}" << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "  DESCRIPTION" << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "   Produces an amalgamation of {inputFile} by replacing #include statements with" << "\n";
-    std::cout << "   the contents of the file they refer to. This replacement will only occur if" << "\n";
-    std::cout << "   the file was located in the same directory, or one of the additional include" << "\n";
-    std::cout << "   paths added with the -i option." << "\n";
-    std::cout << "   " << "\n";
-    std::cout << "   Files included in angle brackets (system includes) are only inlined if the" << "\n";
-    std::cout << "   -s option is specified." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "   If an #include line contains a macro instead of a string literal, the list" << "\n";
-    std::cout << "   of definitions provided through the -d option is consulted to convert the" << "\n";
-    std::cout << "   macro into a string." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "   A file will only be inlined once, with subsequent #include lines for the same" << "\n";
-    std::cout << "   file silently ignored, unless the -f option is specified for the file." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "  OPTIONS" << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -s                Process #include lines containing angle brackets (i.e." << "\n";
-    std::cout << "                      system includes). Normally these are not inlined." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -w {wildcards}    Specify a comma separated list of file name patterns to" << "\n";
-    std::cout << "                      match when deciding to inline (assuming the file can be" << "\n";
-    std::cout << "                      located). The default setting is \"*.cpp;*.c;*.h;*.mm;*.m\"." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -f {file|macro}   Force reinclusion of the specified file or macro on" << "\n";
-    std::cout << "                      all appearances in #include lines." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -p {file|macro}   Prevent reinclusion of the specified file or macro on" << "\n";
-    std::cout << "                      subsequent appearances in #include lines." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -d {name}={file}  Use {file} for macro {name} if it appears in an #include" << "\n";
-    std::cout << "                      line." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -i {dir}          Additionally look in the specified directory for files when" << "\n";
-    std::cout << "                      processing #include lines." << "\n";
-    std::cout << "  " << "\n";
-    std::cout << "    -v                Verbose output mode" << "\n";
-    std::cout << "\n";
-  }
 
-  return error ? 1 : 0;
+
+
+	if (dwChangeHandles[0] == INVALID_HANDLE_VALUE)
+	{
+		printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Watch the subtree for directory creation and deletion. 
+
+	dwChangeHandles[1] = FindFirstChangeNotification(
+		lpDir,                       // directory to watch 
+		TRUE,                          // watch the subtree 
+		FILE_NOTIFY_CHANGE_LAST_WRITE);  // watch dir name changes 
+
+	if (dwChangeHandles[1] == INVALID_HANDLE_VALUE)
+	{
+		printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+		ExitProcess(GetLastError());
+	}
+
+
+	// Make a final validation check on our handles.
+
+	if ((dwChangeHandles[0] == NULL) || (dwChangeHandles[1] == NULL))
+	{
+		printf("\n ERROR: Unexpected NULL from FindFirstChangeNotification.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Change notification is set. Now wait on both notification 
+	// handles and refresh accordingly. 
+
+	while (TRUE)
+	{
+		// Wait for notification.
+
+		printf("\nWaiting for notification...\n");
+
+		dwWaitStatus = WaitForMultipleObjects(2, dwChangeHandles,
+			FALSE, INFINITE);
+
+		switch (dwWaitStatus)
+		{
+		case WAIT_OBJECT_0:
+
+			// A file was created, renamed, or deleted in the directory.
+			// Refresh this directory and restart the notification.
+
+			RefreshDirectory(lpDir);
+			if (FindNextChangeNotification(dwChangeHandles[0]) == FALSE)
+			{
+				printf("\n ERROR: FindNextChangeNotification function failed.\n");
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_OBJECT_0 + 1:
+
+			// A directory was created, renamed, or deleted.
+			// Refresh the tree and restart the notification.
+			return;
+			RefreshTree(lpDir);
+			if (FindNextChangeNotification(dwChangeHandles[1]) == FALSE)
+			{
+				printf("\n ERROR: FindNextChangeNotification function failed.\n");
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_TIMEOUT:
+			return;
+			// A timeout occurred, this would happen if some value other 
+			// than INFINITE is used in the Wait call and no changes occur.
+			// In a single-threaded environment you might not want an
+			// INFINITE wait.
+
+			printf("\nNo changes in the timeout period.\n");
+			break;
+
+		default:
+			printf("\n ERROR: Unhandled dwWaitStatus.\n");
+			ExitProcess(GetLastError());
+			break;
+		}
+	}
+}
+
+void RefreshDirectory(LPTSTR lpDir)
+{
+	// This is where you might place code to refresh your
+	// directory listing, but not the subtree because it
+	// would not be necessary.
+
+	_tprintf(TEXT("Directory (%s) changed.\n"), lpDir);
+}
+
+void RefreshTree(LPTSTR lpDrive)
+{
+	// This is where you might place code to refresh your
+	// directory listing, including the subtree.
+
+	_tprintf(TEXT("Directory tree (%s) changed.\n"), lpDrive);
 }
